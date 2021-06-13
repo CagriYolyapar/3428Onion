93543095c8534ff3ad232e255ffa7103213acb86
Cagri Yolyapar <cagri.yolyapar@gmail.com>
2021-06-13 13:52:17 +0300

    ServiceExtension class'ları tanımlandı

---
+++ Project.BLL/ServiceExtensions/DbContextService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Project.DAL.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.BLL.ServiceExtensions
+{
+    //DbContextPool'umuz böylece StartUp'da DAL'deki bir class'i kullanmadan, sadece BLL'de bu class'ı kullanarak entegre edilecek... Bu sınıfımızın amacı DbContext servisimizi UI'i DAL'e bulastırmadan entegre etmektir...
+    public static class DbContextService
+    {
+        //BU tarz katman bagımsız servis Dependency Injection'larında servislerin icerisine entegre edebileceginiz bir yapı kurmak icin IServiceCollection tipinden yararlanırsınız...
+
+        public static IServiceCollection AddDbContextService(this IServiceCollection services)
+        {
+            ServiceProvider provider = services.BuildServiceProvider();
+
+            //Sakın IConfiguration kullanırken Castle kütüphanesini kullanmayın..Kullanacagınız kütüphane Microsoft.Extensions.Configurations olmak zorundadır...
+            IConfiguration configuration = provider.GetService<IConfiguration>();
+            services.AddDbContextPool<MyContext>(options => options.UseSqlServer(configuration.GetConnectionString("MyConnection")).UseLazyLoadingProxies());
+            return services;
+        }
+    }
+}
---
+++ Project.BLL/ServiceExtensions/RepManExtensionService.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Project.BLL.ManagerServices.Abstracts;
+using Project.BLL.ManagerServices.Concretes;
+using Project.DAL.Repositories.Abstracts;
+using Project.DAL.Repositories.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.BLL.ServiceExtensions
+{
+    public static class RepManExtensionService
+    {
+        public static IServiceCollection AddRepManServices(this IServiceCollection services)
+        {
+            //Repositories
+            //Transient,Scoped,Singleton => Dependency Injection calısırken ilgili class'ların instance'larının ne şekilde alınacağını belirleyen yapılardır...
+
+
+            //Services kısmında AddSingleton metodu size Singleton Pattern ile belirlediginiz sınıfı sunar...Proje kapanana kadar ilgili instance Ram'de kalır,yeni bir tane yaratılmaz var olan kullanılır...
+
+
+            //Scoped => Services kısmında AddScoped kullanılır ise bir Request icin sadece bir instance alınması saglanır...Ancak farklı request'lerde farklı instance alınır....
+
+
+            /*
+             
+             public Category(ICategoryRepository catRep,ICategoryRepository catRep2)
+            {
+            }
+             
+             public IActionResult Deneme(ICategoryRepository catRep)
+            {
+            }
+
+            public IActionResult Deneme2(){}
+             
+
+
+            //Transient => Services kısmında Transient'e eklenen her tip projede her görüldüğü anda instance'i alınabilecek bir hale gelir
+
+
+             */
+
+            //Repositories
+
+            //BaseRepository'nin kesinlikle Abstract olmamalı..Yoksa Dependency Injection BaseRepository'nin instance'ini alamaz ve ilgili Interface'e gönderemez...
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IAppUserRepository, AppUserRepository>();
+            services.AddScoped<IProfileRepository, ProfileRepository>();
+            services.AddScoped<IOrderRepository,OrderRepository>();
+            services.AddScoped<IOrderDetailRepository, OrderDetailRepository>();
+
+            //Managers
+            services.AddScoped(typeof(IManager<>), typeof(BaseManager<>));
+            services.AddScoped<IProductManager, ProductManager>();
+            services.AddScoped<ICategoryManager, CategoryManager>();
+            services.AddScoped<ISpecialUserManager, SpecialUserManager>();
+            services.AddScoped<IProfileManager, ProfileManager>();
+            services.AddScoped<IOrderManager, OrderManager>();
+            services.AddScoped<IOrderDetailManager, OrderDetailManager>();
+
+            return services;
+
+
+        }
+    }
+}
